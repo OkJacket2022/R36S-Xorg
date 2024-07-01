d1fb7c0edd63d892850f17ca5f76b8a7e4433dcc
OkJacket2022 <58190140+OkJacket2022@users.noreply.github.com>
2024-07-01 11:54:48 -0300

    XFCE pulseaudio fix

--- XFCE/Launch-XFCE.sh
+++ XFCE/Launch-XFCE.sh
@@ -8,6 +8,8 @@ printf "\033c" > $CUR_TTY
 
 echo "Starting XFCE."  > $CUR_TTY
 sudo swapon $progdir/swapfile
+sudo mkdir /run/user/1002
+sudo chown 1002 /run/user/1002/
 pulseaudio --start
 startx /usr/bin/startxfce4 -- :1 vt1 > $CUR_TTY 2>&1
 sudo swapoff -a
